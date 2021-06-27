b4787dea038dc12deb2e71d1029f6b7bebea273c
lokesh234 <lgangaraamaney@wpi.edu>
2021-06-27 18:11:04 -0500

    adding header component and writing sign-in component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {Switch, Route} from 'react-router-dom';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from './component/header/header.component.jsx'
 
 function App() {
     return (
         <>
+            <Header />
             <Switch>
                 <Route exact path={'/'} component={HomePage}/>
                 <Route path={'/shop'} component={ShopPage}/>
