7e4930629d8570e38825769f58ae4dabd3cdde3e
lokesh234 <lgangaraamaney@wpi.edu>
2021-06-27 17:51:38 -0500

    updating our app and adding styling to components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
-  return (
-      <>
-        <HomePage/>
-      </>
-  );
+    return (
+        <>
+            <Switch>
+                <Route exact path={'/'} component={HomePage}/>
+                <Route path={'/shop'} component={ShopPage}/>
+            </Switch>
+        </>
+    );
 }
 
 export default App;
